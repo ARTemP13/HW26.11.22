356d290ce92598e171310e4ff74c0d4325aacbfb
ARTemP13 <112979918+ARTemP13@users.noreply.github.com>
2022-12-03 12:10:57 +0300

    Update BankAccount.cs

--- Tumakov№2/BankAccount.cs
+++ Tumakov№2/BankAccount.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,71 +6,76 @@ using System.Threading.Tasks;
 
 namespace Tumakov_2
 {
+    public enum Type
+    {
+        Actual,
+        Save
+    }
     public class BankAccount
     {
-        //public int AccountNumber { get; }
-        //public decimal Balance { get; }
-        //public AccountType TypeAccount { get; }
+        
+        public int AccountNumber { get; set; }
+        public double Balance { get; set; }
+        public Type AccountType { get; set; }
+        
+        
 
-        //enum AccountType
-        //{
-        //    Actual,
-        //    Savings
-        //}
-
-        //public BankAccount(int AccountNumber, decimal Balance, AccountType AccountType)
-        //{
-        //    this.AccountNumber = AccountNumber;
-        //    this.Balance = Balance;
-        //    this.AccountType = AccountType;
-        //}
-        //public override bool Equals(object obj)
-        //{
-        //    if (obj is BankAccount)
-        //    {
-        //        BankAccount ba1 = obj as BankAccount;
-        //        return ba1.Balance == Balance && ba1.AccountType == AccountType;
-        //    }
-        //    else
-        //    {
-        //        return false;
-        //    }
-        //}
+        public BankAccount(int AccountNumber, double Balance, Type AccountType)
+        {
+            this.AccountNumber = AccountNumber;
+            this.Balance = Balance;
+            this.AccountType = AccountType;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is BankAccount)
+            {
+                BankAccount bank1 = obj as BankAccount;
+                bool a =  bank1.Balance == Balance && bank1.AccountType == AccountType;
+                return a;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
 
 
-        //public static bool operator ==(BankAccount ba1, BankAccount ba2)
-        //{
-        //    if ((ba1.Balance == ba2.Balance) && (ba1.AccountType == ba2.AccountType))
-        //    {
-        //        return true;
-        //    }
-        //    else
-        //    {
-        //        return false;
-        //    }
-        //}
+        public static bool operator == (BankAccount bank1, BankAccount bank2)
+        {
+            if ((bank1.Balance == bank2.Balance) && (bank1.AccountType == bank2.AccountType))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
-        //public static bool operator !=(BankAccount ba1, BankAccount ba2)
-        //{
-        //    if ((ba1.Balance != ba2.Balance) && (ba1.AccountType != ba2.AccountType))
-        //    {
-        //        return true;
-        //    }
-        //    else
-        //    {
-        //        return false;
-        //    }
-        //}
+        public static bool operator != (BankAccount bank1, BankAccount bank2)
+        {
+            if ((bank1.Balance != bank2.Balance) && (bank1.AccountType != bank2.AccountType))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
-        //public override int GetHashCode()
-        //{
-        //    return AccountNumber;
-        //}
+        public override int GetHashCode()
+        {
+            return AccountNumber;
+        }
 
-        //public override string ToString()
-        //{
-        //    return $"Account number: {AccountNumber}\n Balance: {Balance}\n Account Type: {AccountType}";
-        //}
+        public override string ToString()
+        {
+            Console.WriteLine("Информация о аккаунте получена");
+            return $"Номер аккаунта: {AccountNumber}\nБаланс: {Balance}\nТип аккаунта: {AccountType}";
+        }
     }
+    
 }
