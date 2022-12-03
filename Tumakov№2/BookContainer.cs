796e3361031745f94d09d051b3fd4c972ac772f8
ARTemP13 <112979918+ARTemP13@users.noreply.github.com>
2022-12-03 12:12:04 +0300

    Update BookContainer.cs

--- Tumakov№2/BookContainer.cs
+++ Tumakov№2/BookContainer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,27 +8,55 @@ namespace Tumakov_2
 {
     internal class BookContainer
     {
-        public List<Book> SortByName(List<Book> books)
+        public static List<Book> SortName(List<Book> books)
         {
             for(int i = 0; i < books.Count; i++)
             {
                 for(int j = 0; j < books.Count; j++)
                 {
-                    if(books[i].Name == books[j].Name)
+                    if ((books[i].Name)[0] > (books[j].Name)[0])
                     {
-
+                        string x = (books[i].Name);
+                        books[i].Name = books[j].Name;
+                        books[j].Name = x;
                     }
                 }
             }
-            return 
+            return books;
         }
-        public List<Book> SortByAuthor(List<Book> books)
+        public static List<Book> SortAuthor(List<Book> books)
         {
-            return books.OrderBy(book => book.Author).ToList();
+            for (int i = 0; i < books.Count; i++)
+            {
+                for (int j = 0; j < books.Count; j++)
+                {
+                    if ((books[i].Author)[0] > (books[j].Author)[0])
+                    {
+                        string x = (books[i].Author);
+                        books[i].Author = books[j].Author;
+                        books[j].Author = x;
+                    }
+                }
+            }
+            return books;
         }
-        public List<Book> SortByPublish(List<Book> books)
+        public static List<Book> SortPublish(List<Book> books)
         {
-            return books.OrderBy(book => book.Publisher).ToList();
+            for (int i = 0; i < books.Count; i++)
+            {
+                for (int j = 0; j < books.Count; j++)
+                {
+                    if ((books[i].Publisher)[0] > (books[j].Publisher)[0])
+                    {
+                        string x = (books[i].Publisher);
+                        books[i].Publisher = books[j].Publisher;
+                        books[j].Publisher = x;
+                    }
+                }
+            }
+            return books;
         }
+
     }
 }
+
